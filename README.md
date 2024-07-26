38832bdec937a231aa25abbb686683b7a7a1f98a
Vinny Ngo <80807250+VinnyNgo03@users.noreply.github.com>
2024-07-26 01:30:22 -0700

    added image generation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Discord Chatbot utilizing text generated from DialoGPT.
+Discord Chatbot utilizing text generation from DialoGPT and image generation from Stable Diffusion.
 
 LLM model loaded using Hugging Face Transformers library.
--- main.py
+++ main.py
@@ -6,21 +6,33 @@ import torch
 import numpy as np
 import pandas as pd
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from diffusers import StableDiffusionPipeline, FlaxStableDiffusionPipeline
 import random
+
 
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else "cpu"
 
-CHECKPOINT = "microsoft/DialoGPT-large"
-tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
-model = AutoModelForCausalLM.from_pretrained(CHECKPOINT, pad_token_id=tokenizer.eos_token_id).to(DEVICE)
+# MODEL LOADING
+gpt_tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
+gpt_model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large", pad_token_id=gpt_tokenizer.eos_token_id).to(DEVICE)
+stdf_model = "CompVis/stable-diffusion-v1-4"
+pipe = StableDiffusionPipeline.from_pretrained(stdf_model, torch_dtype=torch.float16)
+pipe = pipe.to(DEVICE)
+pipe.enable_attention_slicing()
+
 
 def generate_response(input):
-    _input = tokenizer.encode(input + tokenizer.eos_token, return_tensors="pt").to(DEVICE)
-    output = model.generate(_input, max_length=200, pad_token_id=tokenizer.eos_token_id, do_sample=True, top_k=3)
-    return (tokenizer.decode(output[:, _input.shape[-1]:][0], skip_special_tokens=True))
+    _input = gpt_tokenizer.encode(input + gpt_tokenizer.eos_token, return_tensors="pt").to(DEVICE)
+    output = gpt_model.generate(_input, max_length=200, pad_token_id=gpt_tokenizer.eos_token_id, do_sample=True, top_k=3)
+    return (gpt_tokenizer.decode(output[:, _input.shape[-1]:][0], skip_special_tokens=True))
+
+def generate_picture(input):
+    image = pipe(input).images[0]
+    image.save("temp.png")
+    return "temp.png"
 
 def roll(number):
     return random.randint(1, int(number)) if number.isdigit() and number != "0" else "Cannot Roll"
@@ -41,6 +53,13 @@ class MyClient(discord.Client):
             user_message = user_message[3:]
             bot_response = generate_response(user_message)
             await message.channel.send(bot_response)
+
+        if user_message.startswith("!p"):
+            user_message = user_message[3:]
+            bot_response = generate_picture(user_message)
+            picture = discord.File(bot_response)
+            await message.channel.send(file=picture)
+            os.remove(bot_response)
 
         if user_message.startswith("!r"):
             user_message = user_message[3:]
