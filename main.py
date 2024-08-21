64dbcd7874a66d35c1b1cb9c65e4aa6174337b06
Vinny Ngo <80807250+VinnyNgo03@users.noreply.github.com>
2024-08-20 17:37:15 -0700

    added chat history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-from discord import Intents, Client, Message
 import discord
 from discord.ext import commands
 import torch
@@ -14,58 +13,25 @@ import random
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else "cpu"
-# Meta-Llama-3-8B-Instruct
-model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(
-    model_id,
-    torch_dtype = torch.bfloat16,
-    device_map = "auto"
-)
+
 
 # Dialogpt
-gpt_tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-medium")
-gpt_model = AutoModelForCausalLM.from_pretrained("rigby_v2", pad_token_id=gpt_tokenizer.eos_token_id).to(DEVICE)
+tokenizer = AutoTokenizer.from_pretrained("microsoft/DialoGPT-large")
+model = AutoModelForCausalLM.from_pretrained("microsoft/DialoGPT-large", pad_token_id=tokenizer.eos_token_id).to(DEVICE)
+
+chat_history = []
 
 def generate_dialog(input):
-    _input = gpt_tokenizer.encode(input + gpt_tokenizer.eos_token, return_tensors="pt").to(DEVICE)
-    output = gpt_model.generate(_input, max_length=200, pad_token_id=gpt_tokenizer.eos_token_id, do_sample=True, top_k=3)
-    return (gpt_tokenizer.decode(output[:, _input.shape[-1]:][0], skip_special_tokens=True))
+    global chat_history
+    new_input_ids = tokenizer.encode(input + tokenizer.eos_token, return_tensors='pt').to(DEVICE)
+    all_input_ids = torch.cat([chat_history, new_input_ids], dim=-1) if len(chat_history) != 0 else new_input_ids
+    chat_history = model.generate(all_input_ids, max_length=1000, pad_token_id=tokenizer.eos_token_id, do_sample=True, top_k=3)
 
-
-def generate_response(input):
-    messages = [
-        {"role": "system", "content": "You are a serious chatbot who gives concise responses"},
-        {"role": "user", "content": input}
-    ]
-
-    input_ids = tokenizer.apply_chat_template(
-        messages,
-        add_generation_prompt=True,
-        return_tensors="pt"
-    ).to(model.device)
-
-    terminators = [
-        tokenizer.eos_token_id,
-        tokenizer.convert_tokens_to_ids("<|eot_id|>")
-    ]
-
-    outputs = model.generate(
-        input_ids,
-        max_new_tokens=128,
-        eos_token_id=terminators,
-        do_sample=True,
-        temperature=0.8,
-        top_p=0.9
-    )
-
-    response = outputs[0][input_ids.shape[-1]:]
-    return (tokenizer.decode(response, skip_special_tokens=True))
+    return (tokenizer.decode(chat_history[:, all_input_ids.shape[-1]:][0], skip_special_tokens=True))
 
 
 def roll(number):
     return random.randint(1, int(number)) if number.isdigit() and number != "0" else "Cannot Roll"
-
 
 
 intents = discord.Intents.default()
@@ -78,12 +44,12 @@ async def on_ready():
     print('------')
 
 @bot.command()
-async def chat(ctx, *, arg: generate_response):
-    await ctx.send(arg)
-
-@bot.command()
-async def diag(ctx, *, arg: generate_dialog):
-    await ctx.send(arg)
+async def diag(ctx, *, arg):
+    response = generate_dialog(arg)
+    with open('chat_history.txt', 'a') as file:
+        file.write(arg + '\n')
+        file.write(response + '\n')
+    await ctx.send(response)
 
 @bot.command()
 async def roll(ctx, arg: roll):
